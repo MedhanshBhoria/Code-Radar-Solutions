0a7718772bc571d5ef5430f724fc770c135eee32
MedhanshBhoria <medhanshbhoria1048@gmail.com>
2025-02-16 16:15:36 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -18,8 +18,8 @@ int main(){
         case 'F':
             printf("Fail");
             break;
-        default:
-            printf("Invalid Grade");
+    default:
+        printf("Invalid Grade");
     }
     return 0;
 }
